b3cb9dfd765ce07ac561fdbc9eb184b86c47a076
FeBalla <fballadares@uc.cl>
2022-12-26 01:57:15 -0300

    fetching GraphQL endpoint in HyGraph from JSON file

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
 from utils import get_template_query, make_query
 from dotenv import dotenv_values
+import json
+
+# PARAMETERS
+model_name = "Game"
+variable_types_path = "variableTypes.json"
+variables_data_path = "variablesData.json"
+
 
 if __name__ == "__main__":
+  with open(variable_types_path) as f:
+    variable_types = json.load(f)
+  with open(variables_data_path) as f:
+    variables_data = json.load(f)
+
   config = dotenv_values(".env")
-
-  query = """
-  mutation CreateGame(
-    $name: String!,
-    $description: String!,
-    $objectives: String!,
-    $imgUrl: String!,
-  ) {
-    createGame(data: {
-      name: $name,
-      description: $description,
-      objectives: $objectives,
-      imgUrl: $imgUrl
-    }) {
-      id
-      name
-      description
-      objectives
-      imgUrl
-    }
-  }
-  """
-
   endpoint_url = config["CONTENT_API_URL"]
   headers = { 
     "Content-Type": "application/json",
     "Authorization" : f"Bearer {config['PERMANENT_AUTH_TOKEN']}" 
   }
 
-  variables = {
-    "name": "name 1",
-    "description": "description 1",
-    "objectives": "objective 1",
-    "imgUrl": "https://bitacoras-ljyr.s3.amazonaws.com/imagenes/30.png"
-  }
-
-  make_query(endpoint_url, query, variables, headers)
+  query = get_template_query(model_name, variable_types)
+  for entry in variables_data:
+    make_query(endpoint_url, query, entry, headers)
--- utils.py
+++ utils.py
@@ -1,9 +1,64 @@
 import requests
 
-def get_template_query(model_name: str, variables: str) -> str:
-  pass
+def get_template_query(model_name: str, variable_types: str) -> str:
+  """
+  Get the template query for creating a new model entry using the default mutation query generated 
+  by HyGraph. It has the following example format:
+
+  Attributes:
+    `model_name`: The name of the HyGraph model, i.e. `Product`
+
+    `variable_types`: Dictionary with the variable GraphQL types, i.e `{"name": "String!"}`
+  ```
+    mutation CreateModel(
+      $name: String!,
+      $description: String!,
+    ) {
+      createModel(data: {
+        name: $name,
+        description: $description,
+      }) {
+        id
+        name
+        description
+      }
+    }
+    ```
+  """
+  # Adds the first part of the mutation query, naming it as CreateModel (capitalized)
+  query = """mutation """
+  query += f"Create{model_name.capitalize()}(\n"
+  
+  # Adds the query types
+  for variable_name, variable_type in variable_types.items():
+    query += f"  ${variable_name}: {variable_type},\n"
+
+  # Uses the default mutation name for creating entries in HyGraph -> createModel
+  query += ") {\n"
+  query += f"  create{model_name.capitalize()}(data: {{\n"
+
+  # Adds the query variables
+  for variable_name in variable_types.keys():
+    query += f"    {variable_name}: ${variable_name},\n"
+
+  query += "  }) {\n"
+  query += "    id\n"
+
+  # Adds the query fields that will be returned. In this case, all given fields will be fetched
+  for variable_name in variable_types.keys():
+    query += f"    {variable_name}\n"
+
+  query += "  }\n"
+  query += "}"
+  return query
+
 
 def make_query(url: str, query: str, variables: dict = {}, headers: dict = {}) -> None:
-    """Make query to the default GraphQL endpoint from HyGrap with the specified variables"""
+    """
+    Makes query to the default GraphQL endpoint from HyGrap with the specified variables
+    """
     request = requests.post(url, json={ "query": query, "variables": variables }, headers=headers)
-    print(request.json())
+    if request.status_code == 200:
+        return request.json()
+    else:
+      raise Exception(f"Query failed to run by returning code of {request.status_code}:\n{query}")
